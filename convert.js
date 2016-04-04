e3ad11ca4df6813739de396bd0e3d04a0acfae57
ZeframCochran <Anonygram@gmail.com>
2016-04-04 15:39:24 -0700

    Minimal error checking.

--- convert.js
+++ convert.js
@@ -1,14 +1,10 @@
 function convert(path){
   if(!path){return undefined;}
+
   var identifierArray = path.split('.');
-  
-  var obj = this[identifierArray[0]];
-  if(!obj){return undefined;}
   identifierArray.shift();
-  identifierArray.join('');
-  
-  return '$.' + identifierArray.join('');
-  
+
+  return '$.' + identifierArray.join('.');
 }
 
 function o(str){
